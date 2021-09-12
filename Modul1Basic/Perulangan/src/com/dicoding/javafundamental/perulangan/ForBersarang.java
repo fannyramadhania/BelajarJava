023f9eda283da1b90749eadf78462524c1911f18
fanny ramadhania <89308836+fannyramadhania@users.noreply.github.com>
2021-09-12 13:25:54 +0700

    Update ForBersarang.java

--- Modul1Basic/Perulangan/src/com/dicoding/javafundamental/perulangan/ForBersarang.java
+++ Modul1Basic/Perulangan/src/com/dicoding/javafundamental/perulangan/ForBersarang.java
@@ -5,7 +5,7 @@ public class ForBersarang {
         int a = 5;
         for (int i = 0; i <= a; i++) {
             for (int j = 0; j <= i; j++) {
-                System.out.print("*");
+                System.out.print("**");
             }
             System.out.println("");
         }
